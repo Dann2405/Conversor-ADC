9f53369f49610cff467ee2d3241ca3cac93eb379
Dann2405 <danielsoares0558@gmail.com>
2025-02-15 21:10:32 -0300

    feat: arquivo bitlioteca.h adicionado para uma melhor organização

---
+++ include/biblioteca.h
@@ -0,0 +1,19 @@
+#ifndef BIbLIOTECA_H
+#define BIbLIOTECA_H
+
+// Cabeçalho responsavel por incluir todas as bibliotecas necessárias para o projeto 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+
+// cabeçalhos das bibliotecas do display OLED SSD1306
+#include "include/ssd1306/font.h"
+#include "include/ssd1306/ssd1306.h"
+#include "include/ssd1306/configDisplay.h"
+
+
+
+#endif // BIbLIOTECA_H
