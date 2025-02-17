1782a4725b020102de44670bfa0b2e129f81cc6a
Dann2405 <danielsoares0558@gmail.com>
2025-02-16 23:01:20 -0300

    feat: Todos headers do projeto foram incluidos nesse arquivo para facilitar organização

--- include/biblioteca.h
+++ include/biblioteca.h
@@ -1,19 +1,24 @@
-#ifndef BIbLIOTECA_H
-#define BIbLIOTECA_H
+#ifndef BIBLIOTECA_H
+#define BIBLIOTECA_H
 
-// Cabeçalho responsavel por incluir todas as bibliotecas necessárias para o projeto 
+// Arquivo de cabeçalho responsável por incluir todas as bibliotecas necessárias para o projeto
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
+#include "hardware/adc.h"
+#include "hardware/pwm.h"
+#include "hardware/timer.h"
 
-// cabeçalhos das bibliotecas do display OLED SSD1306
+// Cabeçalhos para as bibliotecas do display OLED SSD1306
 #include "include/ssd1306/font.h"
 #include "include/ssd1306/ssd1306.h"
 #include "include/ssd1306/configDisplay.h"
 
+// Cabeçalhos para as bibliotecas do joystick
+#include "include/button.h"
+#include "include/joystick.h"
 
-
-#endif // BIbLIOTECA_H
+#endif // BIBLIOTECA_H
